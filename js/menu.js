4ee8870ed6007dc1e24b6f3fe1c6c2863f2983c9
codeBuilders2022 <codebuilders2022@gmail.com>
2024-08-11 21:35:27 -0600

    menu

---
+++ js/menu.js
@@ -0,0 +1,55 @@
+const tacos = [
+    { title: "PASTOR", description: "Marinated pork in special sauce, onions, cilantro, lime, and tomatillo hot sauce.", price: "$4.99" },
+    { title: "SWEET CHICK", description: "Sweet and spicy marinated chicken, topped with feta cheese, cilantro, lime and tomatillo hot sauce.", price: "$4.99" },
+    { title: "POLLO", description: "Marinated chicken, onions, cilantro and feta cheese.", price: "$4.99" },
+    { title: "CARNE ASADA", description: "Grilled Angus steak, onions, cilantro, lime, and tomatillo hot sauce.", price: "$4.99" },
+    { title: "SHRIMP", description: "Shrimp gild to perfection, lettuce, slaw, chipotle sauce and lime.", price: "$5.99" },
+    { title: "PESCADO", description: "Fish tacos, lettuce, coleslaw, chipotle sauce and lime.", price: "$5.99" },
+    { title: "CARNITAS", description: "Roasted pork with special mango and pineapple sauce, lime and tomatillo hot sauce.", price: "$4.99" },
+    { title: "TACOS DE COCHINITA PIBIL", description: "Yucatan style marinated and baked pork. Served with three flour tortillas, rice and beans, lettuce, guacamole, sour cream, pico de gallo.", price: "$15.99" },
+    { title: "QUESABIRRIA TACOS", description: "Three Mozzarella cheese quesadillas filled with guajillo slow roasted meat. Served with its own beef broth, onion, cilantro, lime and spicy tomatillo sauce.", price: "$15.99" }
+];
+
+const container = document.getElementById('taco_container');
+tacos.forEach(taco => {
+    const tacoInfo = document.createElement('div');
+    tacoInfo.classList.add('taco_info');
+
+    const tacoInfoLeft = document.createElement('div');
+    tacoInfoLeft.classList.add('taco_info_left');
+
+    const title = document.createElement('h2');
+    title.id = 'title';
+    title.textContent = taco.title;
+
+    const description = document.createElement('p');
+    description.classList.add('taco_description');
+    description.textContent = taco.description;
+
+    tacoInfoLeft.appendChild(title);
+    tacoInfoLeft.appendChild(description);
+
+    const price = document.createElement('div');
+    price.id = 'taco_price';
+    price.classList.add('taco_price');
+
+    // Separar el precio en signo de dólar y parte decimal
+    const [dollarSign, priceDecimal] = taco.price.split('.');
+
+    // Crear un span para la parte decimal
+    price.innerHTML = `${dollarSign}.<span class="span_tacos">${priceDecimal}</span>`;
+
+    tacoInfo.appendChild(tacoInfoLeft);
+    tacoInfo.appendChild(price);
+
+    container.appendChild(tacoInfo);
+});
+
+function toggleMenu() {
+    var navbar = document.getElementById('navbar');
+    if (navbar.style.display === "none" || navbar.style.display === "") {
+        navbar.style.display = "flex";
+    } else {
+        navbar.style.display = "none";
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const data = [
-    {"title": "Steak & Shrimp", "img": "img/ESPECIALES WEB/carne asada.jpg", 'alt': "carne_asada_1", 'info': "Steak and shrimp, covered with melted cheese. Served with rice, refried beans and tortillas."},
-    {"title": "Carne Asada", "img": "img/ESPECIALES WEB/carne asada.jpg", 'alt': "carne_asada", 'info': " Grilled skirt steak. Served with rice, refried beans, guacamole salad, pico and tortillas."},
-    {"title": "Chimichanga", "img": "img/ESPECIALES WEB/chimicanga2.jpg", 'alt': "chimicanga", 'info': "Beef Grill Honey Sauce with Paprica"},
-    {"title": "Mexcantina Platte", "img": "img/ESPECIALES WEB/mexcantina plate.jpg", 'alt': "mexcantina_plate", 'info': "Beef Grill Honey Sauce with Paprica"},
-    {"title": "Enchiladas Oaxaqueñas", "img": "img/ESPECIALES WEB/enchilada oaxaca2.jpg", 'alt': "enchilada", 'info': "Beef Grill Honey Sauce with Paprica"},
-    {"title": "Carnitas Michoacan", "img": "img/ESPECIALES WEB/carnitas.jpg", 'alt': "carnitas", 'info': "Beef Grill Honey Sauce with Paprica"},
+    { "title": "Steak & Shrimp", "img": "img/ESPECIALES WEB/carne asada.jpg", 'alt': "carne_asada_1", 'info': "Ribeye steak and shrimp, covered with melted cheese. Served with rice, refried beans and tortillas." },
+    { "title": "Carne Asada", "img": "img/ESPECIALES WEB/carne asada.jpg", 'alt': "carne_asada", 'info': " Grilled skirt steak. Served with rice, refried beans, guacamole salad, pico and tortillas." },
+    { "title": "Chimichanga", "img": "img/ESPECIALES WEB/chimicanga2.jpg", 'alt': "chimicanga", 'info': "Two soft or deep-fried burritos topped with our in-house made cheese sauce. Served with rice and refried beans, sour cream and guacamole salad." },
+    { "title": "Mexcantina Platter", "img": "img/ESPECIALES WEB/mexcantina plate.jpg", 'alt': "mexcantina_plate", 'info': "Chicken enchilada, grilled marinated chicken breast and skirt steak. Served with rice, black beans and tortillas." },
+    { "title": "Enchiladas Oaxaqueñas", "img": "img/ESPECIALES WEB/enchilada oaxaca2.jpg", 'alt': "enchilada", 'info': "Supreme combination of four enchiladas, one chicken, one steak, one cheese and one beans, covered with creamy green tomatillo sauce, lettuce, sour cream and tomato" },
+    { "title": "Carnitas Michoacan", "img": "img/ESPECIALES WEB/carnitas.jpg", 'alt': "carnitas", 'info': "DELICIOUS roasted pork tips, Served with guacamole salad, pico, rice, beans and tortillas." },
 ];
+
 
 function agregarContenedores() {
     // Obtener el contenedor principal por su ID
@@ -53,5 +54,4 @@ function toggleMenu() {
         navbar.style.display = "none";
     }
 }
-
 
